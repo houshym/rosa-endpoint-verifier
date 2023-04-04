471d3b966e0713e1c29d1256b8f9d1a327bab632
houshym <35855105+houshym@users.noreply.github.com>
2023-04-03 19:31:24 -0700

    Update all-in-one.sh

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,19 +1,41 @@
+#This script reads the region and one to three subnet IDs provided by the user, 
+#appends them to a list of subnet IDs, and creates a Terraform variables file with the region and subnet IDs. 
+#It then initializes and applies the Terraform code.
+
 #!/bin/bash
 
 # Read variables
 read -p "Enter the AWS region: " REGION
 read -p "Enter the ID of the first subnet: " SUBNET_ID_1
-read -p "Enter the ID of the second subnet: " SUBNET_ID_2
-read -p "Enter the ID of the third subnet: " SUBNET_ID_3
 
+# Create an empty list of subnet IDs
+SUBNET_IDS=($SUBNET_ID_1)
+
+# Read the second and third subnet IDs if provided
+read -p "Enter the ID of the second subnet (leave empty if not applicable): " SUBNET_ID_2
+read -p "Enter the ID of the third subnet (leave empty if not applicable): " SUBNET_ID_3
+
+# Append the second and third subnet IDs if provided
+if [[ ! -z $SUBNET_ID_2 ]]; then
+  SUBNET_IDS+=($SUBNET_ID_2)
+fi
+
+if [[ ! -z $SUBNET_ID_3 ]]; then
+  SUBNET_IDS+=($SUBNET_ID_3)
+fi
+
+# Create a Terraform variables file
 cat << EOF > terraform.tfvars
 region = "$REGION"
-subnet_ids = ["$SUBNET_ID_1", "$SUBNET_ID_2", "$SUBNET_ID_3"]
+subnet_ids = ${SUBNET_IDS[@]}
 EOF
 
 # Initialize and apply Terraform code
 terraform init
 terraform apply -auto-approve
+
+
+
 
 # Get instance IDs and public IP addresses
 INSTANCE_IDS=$(terraform output instance_ids | tr -d '[],"')
