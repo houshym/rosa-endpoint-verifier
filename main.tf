0322b08ce26ea5f4edd8f449df460e7aaff3d473
houshym <35855105+houshym@users.noreply.github.com>
2023-04-03 19:11:20 -0700

    add user data

--- main.tf
+++ main.tf
@@ -1,21 +1,37 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-resource "aws_instance" "rosa-ep-verifier" {
+resource "aws_instance" "ec2_instance" {
   count = 3
 
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
+  subnet_id     = element(var.subnet_ids, count.index)
 
-  subnet_id = element(var.subnet_ids, count.index)
+  user_data = <<-EOF
+              #!/bin/bash
+              # This script will be run as the ec2-user when the instance launches
 
-  tags = {
-    Name = "example-instance-${count.index + 1}"
-  }
+              # Install necessary packages
+              yum update -y
+              yum install -y nc
+              yum install -y curl
+              yum install -y jq
+
+              # Copy the connectivity test script
+              curl -o /home/ec2-user/connectivity_test.sh https://raw.githubusercontent.com/<username>/<repository>/<branch>/connectivity_test.sh
+              chmod +x /home/ec2-user/connectivity_test.sh
+
+              # Run the connectivity test
+              /home/ec2-user/connectivity_test.sh
+              EOF
 }
 
-variable "subnet_ids" {
-  type    = list(string)
-  default = ["subnet-abc123", "subnet-def456", "subnet-ghi789"]
+output "instance_ids" {
+  value = aws_instance.ec2_instance.*.id
+}
+
+output "public_ips" {
+  value = aws_instance.ec2_instance.*.public_ip
 }
