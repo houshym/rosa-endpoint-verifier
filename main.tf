f3e6b27255954ff3dbe51b411240e270da68e9dc
houshym <35855105+houshym@users.noreply.github.com>
2023-04-03 19:34:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  count = 3
+  count = length(var.subnet_ids)
 
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
@@ -12,18 +12,14 @@ resource "aws_instance" "ec2_instance" {
   user_data = <<-EOF
               #!/bin/bash
               # This script will be run as the ec2-user when the instance launches
-
               # Install necessary packages
               yum update -y
               yum install -y nc
               yum install -y curl
               yum install -y jq
-
               # Copy the connectivity test script
-              curl -o /home/ec2-user/connectivity_test.sh https://raw.githubusercontent.com/houshym/
-rosa-endpoint-verifier/main/rosa-net-verifier.sh
+              curl -o /home/ec2-user/connectivity_test.sh https://raw.githubusercontent.com/houshym/rosa-endpoint-verifier/main/rosa-net-verifier.sh
               chmod +x /home/ec2-user/rosa-net-verifier.sh
-
               # Run the connectivity test
               /home/ec2-user/rosa-net-verifier.sh ${var.region}
               EOF
@@ -31,8 +27,4 @@ rosa-endpoint-verifier/main/rosa-net-verifier.sh
 
 output "instance_ids" {
   value = aws_instance.ec2_instance.*.id
-}
-
-output "public_ips" {
-  value = aws_instance.ec2_instance.*.public_ip
 }
